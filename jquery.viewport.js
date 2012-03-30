a43bffba369f5204c614b2d5aaf0826a9f9123dd
George Pantazis <gpantazis@cpbgroup.com>
2012-03-30 00:12:14 -0600

    Add supporting functions for 'clipped' selectors. Need to expand the heck out of these.

--- jquery.viewport.js
+++ jquery.viewport.js
@@ -11,48 +11,114 @@
  *
  */
 (function($) {
-    
-    $.belowthefold = function(element, settings) {
-        var fold = $(window).height() + $(window).scrollTop();
-        return fold <= $(element).offset().top - settings.threshold;
-    };
 
-    $.abovethetop = function(element, settings) {
-        var top = $(window).scrollTop();
-        return top >= $(element).offset().top + $(element).height() - settings.threshold;
-    };
-    
-    $.rightofscreen = function(element, settings) {
-        var fold = $(window).width() + $(window).scrollLeft();
-        return fold <= $(element).offset().left - settings.threshold;
-    };
-    
-    $.leftofscreen = function(element, settings) {
-        var left = $(window).scrollLeft();
-        return left >= $(element).offset().left + $(element).width() - settings.threshold;
-    };
-    
-    $.inviewport = function(element, settings) {
-        return !$.rightofscreen(element, settings) && !$.leftofscreen(element, settings) && !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
-    };
-    
-    $.extend($.expr[':'], {
-        "below-the-fold": function(a, i, m) {
-            return $.belowthefold(a, {threshold : 0});
-        },
-        "above-the-top": function(a, i, m) {
-            return $.abovethetop(a, {threshold : 0});
-        },
-        "left-of-screen": function(a, i, m) {
-            return $.leftofscreen(a, {threshold : 0});
-        },
-        "right-of-screen": function(a, i, m) {
-            return $.rightofscreen(a, {threshold : 0});
-        },
-        "in-viewport": function(a, i, m) {
-            return $.inviewport(a, {threshold : 0});
-        }
-    });
+	// Boolean Indicators for ':' Selector filters
 
-    
+	$.belowthefold = function(element, settings) {
+		var fold = $(window).height() + $(window).scrollTop();
+		return fold <= $(element).offset().top - settings.threshold;
+	};
+
+	$.abovethetop = function(element, settings) {
+		var top = $(window).scrollTop();
+		return top >= $(element).offset().top + $(element).height() - settings.threshold;
+	};
+
+	$.rightofscreen = function(element, settings) {
+		var fold = $(window).width() + $(window).scrollLeft();
+		return fold <= $(element).offset().left - settings.threshold;
+	};
+
+	$.leftofscreen = function(element, settings) {
+		var left = $(window).scrollLeft();
+		return left >= $(element).offset().left + $(element).width() - settings.threshold;
+	};
+
+	$.inviewport = function(element, settings) {
+		return !$.rightofscreen(element, settings) && !$.leftofscreen(element, settings) && !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
+	};
+
+	$.isclipped = function(element, settings) {
+		var clippedPositionData = methods.clippedPosition(element);
+		return !( clippedPositionData.portionOnScreen === 0 || clippedPositionData.portionOnScreen === 1 );
+	};
+
+	// Methods
+
+	var methods = {
+
+		// TODO : Expand for vertical clipping.
+		clippedPosition : function( element, settings ) {
+			var $el = $(element),
+				$window = $(window);
+
+			var windowWidth = $window.width(),
+				elemWidth = $el.width(),
+				elemViewportLeft = $el.offset().left - $window.scrollLeft();
+
+			var data = {};
+
+			// Object is completely off-screen, return.
+			if ( !$.inviewport(element, {threshold: 0}) ) {
+				data.portionOnScreen = 0;
+				return data;
+			}
+
+			// Overflows on both sides.
+			if ( elemViewportLeft < 0 && elemViewportLeft + elemWidth > windowWidth ) {
+				data.portionOnScreen = windowWidth / elemWidth;
+				data.portionOffScreenLeft = -1 * elemViewportLeft / elemWidth;
+				data.portionOffScreenRight = 1 - data.portionOnScreen - data.portionOffScreenLeft;
+				data.horizontal = 'both';
+
+			// Overflows on the left only.
+			} else if ( elemViewportLeft < 0 ) {
+				data.portionOnScreen = ( elemWidth + elemViewportLeft ) / elemWidth;
+				data.portionOffScreenLeft = 1 - data.portionOnScreen;
+				data.horizontal = 'left';
+
+			// Overflows on the right only.
+			} else if ( elemViewportLeft < windowWidth && elemViewportLeft + elemWidth > windowWidth ) {
+				data.portionOnScreen = ( windowWidth - elemViewportLeft ) / elemWidth;
+				data.portionOffScreenRight = 1 - data.portionOnScreen;
+				data.horizontal = 'right';
+
+			// Does not overflow.
+			} else {
+				data.portionOnScreen = 1;
+			}
+
+			return data;
+		}
+	};
+
+	// EXTEND
+
+	$.fn.clippedPosition = function() {
+		return methods.clippedPosition(this);
+	};
+
+	$.extend($.expr[':'], {
+		"below-the-fold": function(a, i, m) {
+			return $.belowthefold(a, {threshold : 0});
+		},
+		"above-the-top": function(a, i, m) {
+			return $.abovethetop(a, {threshold : 0});
+		},
+		"left-of-screen": function(a, i, m) {
+			return $.leftofscreen(a, {threshold : 0});
+		},
+		"right-of-screen": function(a, i, m) {
+			return $.rightofscreen(a, {threshold : 0});
+		},
+		"in-viewport": function(a, i, m) {
+			return $.inviewport(a, {threshold : 0});
+		},
+		"clipped": function(a, i, m) {
+			return $.isclipped(a, {
+				threshold: 0
+			});
+		}
+	});
+
 })(jQuery);
